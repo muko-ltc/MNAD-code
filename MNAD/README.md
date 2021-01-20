d14eb7a3c1bf8e542c04608a18a39659eaad93c5
muko-ltc <55936509+muko-ltc@users.noreply.github.com>
2021-01-20 13:03:52 +0800

    Add files via upload

---
+++ MNAD/README.md
@@ -0,0 +1,59 @@
+# PyTorch implementation of "Learning Memory-guided Normality for Anomaly Detection"
+
+<p align="center"><img src="./MNAD_files/overview.png" alt="no_image" width="40%" height="40%" /><img src="./MNAD_files/teaser.png" alt="no_image" width="60%" height="60%" /></p>
+This is the implementation of the paper "Learning Memory-guided Normality for Anomaly Detection (CVPR 2020)".
+
+For more information, checkout the project site [[website](https://cvlab.yonsei.ac.kr/projects/MNAD/)] and the paper [[PDF](http://openaccess.thecvf.com/content_CVPR_2020/papers/Park_Learning_Memory-Guided_Normality_for_Anomaly_Detection_CVPR_2020_paper.pdf)].
+
+## Dependencies
+* Python 3.6
+* PyTorch 1.1.0
+* Numpy
+* Sklearn
+
+## Datasets
+* USCD Ped2 [[dataset](https://github.com/StevenLiuWen/ano_pred_cvpr2018)]
+* CUHK Avenue [[dataset](https://github.com/StevenLiuWen/ano_pred_cvpr2018)]
+* ShanghaiTech [[dataset](https://github.com/StevenLiuWen/ano_pred_cvpr2018)]
+
+These datasets are from an official github of "Future Frame Prediction for Anomaly Detection - A New Baseline (CVPR 2018)".
+
+Download the datasets into ``dataset`` folder, like ``./dataset/ped2/``
+
+## Training
+* The training and testing codes are based on prediction method
+```bash
+git clone https://github.com/cvlab-yonsei/projects
+cd projects/MNAD/code
+python Train.py # for training
+```
+* You can freely define parameters with your own settings like
+```bash
+python Train.py --gpus 1 --dataset_path 'your_dataset_directory' --dataset_type avenue --exp_dir 'your_log_directory'
+```
+
+## Pre-trained model and memory items
+* Download our pre-trained model and memory items <br>Link: [[model and items](https://drive.google.com/file/d/11f65puuljkUa0Z4W0VtkF_2McphS02fq/view?usp=sharing)]
+* Note that, these are from training with the Ped2 dataset
+
+## Evaluation
+* Test the model with our pre-trained model and memory items
+```bash
+python Evaluate.py --dataset_type ped2 --model_dir pretrained_model.pth --m_items_dir m_items.pt
+```
+* Test your own model
+* Check your dataset_type (ped2, avenue or shanghai)
+```bash
+python Evaluate.py --dataset_type ped2 --model_dir your_model.pth --m_items_dir your_m_items.pt
+```
+
+## Bibtex
+```
+@inproceedings{park2020learning,
+  title={Learning Memory-guided Normality for Anomaly Detection},
+  author={Park, Hyunjong and Noh, Jongyoun and Ham, Bumsub},
+  booktitle={Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition},
+  pages={14372--14381},
+  year={2020}
+}
+```
---
+++ MNAD/utils.py
@@ -0,0 +1,119 @@
+import numpy as np
+import os
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.utils as v_utils
+import matplotlib.pyplot as plt
+import cv2
+import math
+from collections import OrderedDict
+import copy
+import time
+from sklearn.metrics import roc_auc_score
+
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+def psnr(mse):
+
+    return 10 * math.log10(1 / mse)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def normalize_img(img):
+
+    img_re = copy.copy(img)
+    
+    img_re = (img_re - np.min(img_re)) / (np.max(img_re) - np.min(img_re))
+    
+    return img_re
+
+def point_score(outputs, imgs):
+    
+    loss_func_mse = nn.MSELoss(reduction='none')
+    error = loss_func_mse((outputs[0]+1)/2,(imgs[0]+1)/2)
+    normal = (1-torch.exp(-error))
+    score = (torch.sum(normal*loss_func_mse((outputs[0]+1)/2,(imgs[0]+1)/2)) / torch.sum(normal)).item()
+    return score
+    
+def anomaly_score(psnr, max_psnr, min_psnr):
+    return ((psnr - min_psnr) / (max_psnr-min_psnr))
+
+def anomaly_score_inv(psnr, max_psnr, min_psnr):
+    return (1.0 - ((psnr - min_psnr) / (max_psnr-min_psnr)))
+
+def anomaly_score_list(psnr_list):
+    anomaly_score_list = list()
+    for i in range(len(psnr_list)):
+        anomaly_score_list.append(anomaly_score(psnr_list[i], np.max(psnr_list), np.min(psnr_list)))
+        
+    return anomaly_score_list
+
+def anomaly_score_list_inv(psnr_list):
+    anomaly_score_list = list()
+    for i in range(len(psnr_list)):
+        anomaly_score_list.append(anomaly_score_inv(psnr_list[i], np.max(psnr_list), np.min(psnr_list)))
+        
+    return anomaly_score_list
+
+def AUC(anomal_scores, labels):
+    frame_auc = roc_auc_score(y_true=np.squeeze(labels, axis=0), y_score=np.squeeze(anomal_scores))
+    return frame_auc
+
+def score_sum(list1, list2, alpha):
+    list_result = []
+    for i in range(len(list1)):
+        list_result.append((alpha*list1[i]+(1-alpha)*list2[i]))
+        
+    return list_result
+
+def torchToCv(input):
+    input = input.squeeze(0)
+    inputCv = input.numpy()
+    inputCv = np.transpose(inputCv, (1,2,0))
+    return inputCv
+
+def get_timing_signal_1d(length,
+                         channels,
+                         min_timescale=1.0,
+                         max_timescale=1.0e4,
+                         start_index=0):
+  """Gets a bunch of sinusoids of different frequencies.
+  Each channel of the input Tensor is incremented by a sinusoid of a different
+  frequency and phase.
+  This allows attention to learn to use absolute and relative positions.
+  Timing signals should be added to some precursors of both the query and the
+  memory inputs to attention.
+  The use of relative position is possible because sin(x+y) and cos(x+y) can be
+  expressed in terms of y, sin(x) and cos(x).
+  In particular, we use a geometric sequence of timescales starting with
+  min_timescale and ending with max_timescale.  The number of different
+  timescales is equal to channels / 2. For each timescale, we
+  generate the two sinusoidal signals sin(timestep/timescale) and
+  cos(timestep/timescale).  All of these sinusoids are concatenated in
+  the channels dimension.
+  Args:
+    length: scalar, length of timing signal sequence.
+    channels: scalar, size of timing embeddings to create. The number of
+        different timescales is equal to channels / 2.
+    min_timescale: a float
+    max_timescale: a float
+    start_index: index of first position
+  Returns:
+    a Tensor of timing signals [1, length, channels]
+  """
+  position = torch.arange(start_index,length)
+  num_timescales = channels // 2
+  log_timescale_increment = (math.log(float(max_timescale) / float(min_timescale)) / (num_timescales - 1))
+  inv_timescales = min_timescale * torch.exp(torch.arange(0,num_timescales) * -log_timescale_increment)
+  scaled_time = position.unsqueeze(1) * inv_timescales.unsqueeze(0)
+  signal = torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)], dim=1)
+  signal = torch.nn.functional.pad(signal, [0, channels%2, 0, 0])
+  signal = signal.reshape(1, length, channels).repeat(3,1,1)
+  return signal
